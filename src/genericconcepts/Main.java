e9f8cb1fc0b7627d4f4fb26d98f94c7c64ff4506
Akilan.k <akilanakshu@gmail.com>
2023-09-26 09:34:44 +0530

    GenericConcepts in java

---
+++ src/genericconcepts/Main.java
@@ -0,0 +1,16 @@
+package genericconcepts;
+
+public class Main {
+
+	public static void main(String[] args) {
+		{
+			Test <String, Integer> obj =
+				new Test<String, Integer>("GfG", 15);
+
+			obj.print();
+		}
+	}
+
+	}
+
+
